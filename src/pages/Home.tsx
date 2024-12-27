43ab6d4a697a9fef79196548a856a1cc8355b6b4
Esben Fogh <esbenvensel.fogh@gmail.com>
2024-12-27 22:09:19 +0100

    Dist added to deploy v8

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,16 +15,8 @@ function Home() {
               <h4>Ting du skal have med</h4>
               <ol className="list-group list-group-numbered">
                 <li className="list-group-item">Bærbar med USB 3.0 eller 2.0</li>
-                <li className="list-group-item">Arduino installeret på computeren (Instruksioner nedeunder)</li>
-                <li className="list-group-item">Godt humør :)</li>
+                <li className="list-group-item">Arduino installeret på computeren (Instruksioner <Link to="/forberedelse">her</Link>)</li>
               </ol>
-
-              <br />
-              <h4>Resurser</h4>
-              <br />
-
-              {/* Example of using Link for internal routing */}
-              <Link to="/forberedelse">Arduino installering</Link>
             </Col>
           </Row>
         </Container>
